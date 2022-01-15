f15ba5908b2a8aedacf721ac2da40aef55db63a3
Pavlyuchenko <mmichal.pavlicek@seznam.cz>
2022-01-15 16:56:32 +0100

    Vercel

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
+		sirv("static", { dev }),
+		sapper.middleware({
+			session: (req, res) => ({
+				"user-agent": req.headers["user-agent"],
+			}),
+		})
 	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
+	.listen(PORT, (err) => {
+		if (err) console.log("error", err);
 	});
